3a218155f8d51d51d45129052538d9f411273500
krustles <jordanrpeterson@gmail.com>
2016-10-10 11:57:10 -0600

    made logo a link to landing page

--- webpack/components/Nav.js
+++ webpack/components/Nav.js
@@ -26,7 +26,7 @@ class Nav extends Component {
 				<div className='navbar-fixed'>
 					<nav>
 						<div className='nav-color nav-wrapper'>
-							<img className='hide-on-small-only' src={logoImage} />
+							<IndexLink to='/'><img className='hide-on-small-only' src={logoImage} /></IndexLink>
 							<ul id='nav-mobile' className='right'>
 								<SideMenu logout={this.logout} auth={this.props.auth} />
 								<li><IndexLink to='/' className='hide-on-med-and-down' activeClassName={linkClass}>Home</IndexLink></li>
