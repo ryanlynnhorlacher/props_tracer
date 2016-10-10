2351d261982299060946cd1f5c3c7c7337a8a487
Christian Lisonbee <christian.lisonbee@gmail.com>
2016-10-10 12:16:10 -0600

    model testing

--- spec/models/estimate_spec.rb
+++ spec/models/estimate_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Estimate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe 'validations' do
+  	it { should validate_presence_of (:location) }
+  	it { should validate_presence_of (:distance) }
+  	it { should validate_presence_of (:final_price) }
+  	it { should validate_presence_of (:fence_material) }
+  	it { should validate_presence_of (:fence_height) }
+  	it { should validate_presence_of (:gate_count) }
+  	it { should belong_to (:customer) }
+  end
 end
--- spec/models/material_spec.rb
+++ spec/models/material_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Material, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+  	it { should validate_presence_of :name}
+  	it { have_many :heights}
+  	it { have_many :gate_types}
 end
